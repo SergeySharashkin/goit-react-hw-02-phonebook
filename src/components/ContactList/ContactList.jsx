135219def3039f6eb05030031ff16c9aecb601c3
SergeySharashkin <S_sharashkin@mail.ru>
2022-01-09 04:00:00 +0300

    fff

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,18 +1,20 @@
 import React from "react";
-// import PropTypes from 'prop-types';
+import PropTypes, { shape } from "prop-types";
 import Contact from "./Contact";
 const ContactList = ({ contacts }) => (
   <ul>
     {contacts.map(({ id, name, number }) => (
-      <Contact id={id} name={name} number={number} />
+      <Contact key={id} name={name} number={number} />
     ))}
   </ul>
 );
 export default ContactList;
-// ContactList.propTypes = {
-//   contacts: {
-//     id: PropTypes.string,
-//     name: PropTypes.string,
-//     number: PropTypes.string,
-// }
-// }
+ContactList.propTypes = {
+  contacts: PropTypes.arrayOf(
+    shape({
+      key: PropTypes.string,
+      name: PropTypes.string,
+      number: PropTypes.string,
+    })
+  ),
+};
